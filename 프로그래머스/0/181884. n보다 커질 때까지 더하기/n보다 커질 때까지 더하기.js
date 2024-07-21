aca689f11fcda5e3e8a839b6ea4a48041ed3448b
bys96 <jkpp0611@gmail.com>
2024-07-21 13:39:52 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,10 +1,11 @@
 function solution(numbers, n) {
     var answer = 0;
-    for(let i=0; i<numbers.length; i++){
-        answer += numbers[i];
-        if(answer > n){
-            break;
-        }
-    }
+    // for(let i=0; i<numbers.length; i++){
+    //     answer += numbers[i];
+    //     if(answer > n){
+    //         break;
+    //     }
+    // }
+    answer = numbers.reduce((a,b) => (a>n ? a : a+=b));
     return answer;
 }

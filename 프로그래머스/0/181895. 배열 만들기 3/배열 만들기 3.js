717a6bf5e32b6222e7a0ab0b6fbec9d97eea28e4
bys96 <jkpp0611@gmail.com>
2024-07-29 10:12:06 +0900

    [level 0] Title: 배열 만들기 3, Time: 5.06 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,9 +1,16 @@
+// function solution(arr, intervals) {
+//     var answer = [];
+//     for(let i=0; i<=1; i++){
+//         for(let j=intervals[i][0]; j<=intervals[i][1]; j++){
+//             answer.push(arr[j]);
+//         }
+//     }
+//     return answer;
+// }
+
 function solution(arr, intervals) {
     var answer = [];
-    for(let i=0; i<=1; i++){
-        for(let j=intervals[i][0]; j<=intervals[i][1]; j++){
-            answer.push(arr[j]);
-        }
-    }
+    let [[a,b],[c,d]] = intervals;
+    answer = [...arr.slice(a,b+1),...arr.slice(c,d+1)];
     return answer;
 }

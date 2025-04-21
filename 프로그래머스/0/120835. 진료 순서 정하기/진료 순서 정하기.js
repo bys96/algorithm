846f53e9250ec0e0dbc8f5b70dcf5d12c256013e
bys96 <jkpp0611@gmail.com>
2025-04-21 14:04:05 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -0,0 +1,13 @@
+function solution(emergency) {
+    var answer = [];
+    for(let i=0; i<emergency.length; i++){
+        let count = 1;
+        for(let j=0; j<emergency.length; j++){
+            if(emergency[i]<emergency[j]){
+                count++;
+            }
+        }
+        answer.push(count);
+    }
+    return answer;
+}

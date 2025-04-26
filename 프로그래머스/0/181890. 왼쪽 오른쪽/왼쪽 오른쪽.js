d6547788cba7c4537d1073cd98271bd24e8f1c29
bys96 <jkpp0611@gmail.com>
2025-04-26 19:45:07 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,19 @@
+function solution(str_list) {
+    var answer = [];
+    
+    for(let i=0; i<str_list.length; i++){
+        if(str_list[i]=="l"){
+            answer=str_list.slice(0,i);
+            break;
+        }else if(str_list[i]=="r"){
+            answer=str_list.slice(i+1)
+            break;
+        }
+    }
+    
+    if(!str_list.includes("l") && !str_list.includes("r")){
+        return [];
+    }
+    
+    return answer;
+}

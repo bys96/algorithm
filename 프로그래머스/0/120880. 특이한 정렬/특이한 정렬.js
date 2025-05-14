d4e799074599bd6086e0190befd8c2aca137d687
bys96 <jkpp0611@gmail.com>
2025-05-14 22:46:35 +0900

    [level 0] Title: 특이한 정렬, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,6 @@
+function solution(numlist, n) {
+    var answer = [];
+    
+    answer = numlist.sort((a,b)=> Math.abs(a-n) - Math.abs(b-n) !== 0 ? Math.abs(a-n) - Math.abs(b-n) : b-a );
+    return answer;
+}

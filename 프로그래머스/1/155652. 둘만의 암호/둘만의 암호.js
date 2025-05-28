36cfe9a4f5d68efb0e29a54e2a7bf37cfefe96d6
bys96 <jkpp0611@gmail.com>
2025-05-28 22:56:41 +0900

    [level 1] Title: 둘만의 암호, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,8 @@
+function solution(s, skip, index) {
+    let a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    let answer = '';
+    
+    let b = a.filter(val => !skip.includes(val));
+    answer = s.split("").map(val => val = b.indexOf(val) + index < b.length ? b[b.indexOf(val)+index] : b[(b.indexOf(val)+index)%b.length]).join("");
+    return answer;
+}

fbf5e64a9e5636244c69dc6c5a3150aba9d25858
bys96 <jkpp0611@gmail.com>
2025-07-29 18:09:29 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    var answer = '';
+    let arr = s.split(" ");
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+    answer = min + " " + max;
+    return answer;
+}

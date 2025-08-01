3ba5ff9f12a011b240f887d23deb51774f604143
bys96 <jkpp0611@gmail.com>
2025-08-01 16:47:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 48.66 ms, Memory: 39.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,11 @@
+function solution(s)
+{
+    let arr = [];
+    
+    for(let i=0; i<s.length; i++){
+        if(s[i] == arr[arr.length-1]) arr.pop();
+        else arr.push(s[i]);
+    }
+    
+    return arr.length==0 ? 1 : 0;
+}

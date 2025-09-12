5bc9439efa95603378750bb24a9d264e8d06cc40
bys96 <jkpp0611@gmail.com>
2025-09-12 14:32:12 +0900

    [level 2] Title: 땅따먹기, Time: 47.80 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,16 @@
+function solution(land) {
+    var answer = 0;
+    let n = land.length;
+    
+
+    for(let i=1; i<n; i++){
+        land[i][0] += Math.max(land[i-1][1], land[i-1][2], land[i-1][3]);
+        land[i][1] += Math.max(land[i-1][0], land[i-1][2], land[i-1][3]);
+        land[i][2] += Math.max(land[i-1][0], land[i-1][1], land[i-1][3]);
+        land[i][3] += Math.max(land[i-1][0], land[i-1][1], land[i-1][2]);
+    }
+    
+    answer = Math.max(land[n-1][0],land[n-1][1],land[n-1][2],land[n-1][3]);
+
+    return answer;
+}

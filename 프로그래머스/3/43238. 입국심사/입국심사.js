fdbd43aea3a9856d89d505127225d5f2f4496134
bys96 <jkpp0611@gmail.com>
2025-11-09 23:48:29 +0900

    [level 3] Title: 입국심사, Time: 31.44 ms, Memory: 43.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,25 @@
+function solution(n, times) {
+    var answer = 0;
+    let left = 1;
+    let right = Math.max(...times)*n;
+    answer = right;
+    
+    while(left <= right){
+        let mid = Math.floor((left+right)/2);
+        let total = 0;
+        
+        for(let t of times){
+            total += Math.floor(mid/t);
+            if(total>=n) break;
+        }
+        
+        if(total>=n){
+            answer = mid;
+            right = mid-1;
+        }else{
+            left = mid+1;
+        }
+    }
+    
+    return answer;
+}

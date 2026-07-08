65e882448caa581106c012c3d7d57e65268cb043
bys96 <jkpp0611@gmail.com>
2026-07-08 23:42:36 +0900

    [level 2] Title: 짝지어 제거하기, Time: 97.11 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=java) 
 
 ### 성능 요약
 
-메모리: 39.5 MB, 시간: 48.66 ms
+메모리: 79.8 MB, 시간: 97.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 01일 16:47:00
+2026년 07월 08일 23:42:30
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(String s) {
+        int answer = -1;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        answer = stack.isEmpty() ? 1 : 0;
+
+        return answer;
+    }
+}

db6a46b1465d5d8f1693a4c33c86087c0209e732
bys96 <jkpp0611@gmail.com>
2026-07-23 23:40:19 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.38 ms
+메모리: 86.7 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 19일 16:04:36
+2026년 07월 23일 23:40:15
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int idx = 0;
+
+        while (idx < progresses.length) {
+            for (int i = idx; i < progresses.length; i++) {
+                progresses[i] += speeds[i];
+            }
+            
+            int cnt = 0;
+            
+            while (idx < progresses.length && progresses[idx] >= 100) {
+                cnt++;
+                idx++;
+            }
+
+            if (cnt > 0) {
+                list.add(cnt);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}

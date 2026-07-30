bed2918d04cf7d06847f33c7f66f827ee63ff7d6
bys96 <jkpp0611@gmail.com>
2026-07-30 23:47:08 +0900

    [level 1] Title: 하샤드 수, Time: 4.23 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        String str = x+"0";
+        int sum = 0;
+        
+        for(int i=0; i<str.length(); i++){
+            sum += str.charAt(i)-'0';
+        }
+        
+        if(x%sum==0){
+            answer = true;
+        }else{
+            answer = false;
+        }
+        return answer;
+    }
+}
